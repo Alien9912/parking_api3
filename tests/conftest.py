c417ecc8ae699952cf4b276a4bb4eff98037c4f9
Home_Lab2 <y.nikitos21@gmail.com>
2026-06-04 00:05:54 +0300

    Final fixes: isort, black applied, flake8 passing, tests green

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ from app.models import Client, ClientParking, Parking
 
 @pytest.fixture
 def app():
-    app = create_app(
-        {"TESTING": True, "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"}
-    )
+    app = create_app({"TESTING": True, "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"})
     with app.app_context():
         _db.create_all()
         yield app
